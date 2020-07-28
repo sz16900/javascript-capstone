f204446fe9b7dde5daf11c10d9aef97f1e8ac34d
sz16900 <sz16900@my.bristol.ac.uk>
2020-07-28 17:33:56 -0500

    add spinning wheel

--- src/Scenes/LeaderBoardScene.js
+++ src/Scenes/LeaderBoardScene.js
@@ -7,35 +7,41 @@ export default class GameOverScene extends Phaser.Scene {
   }
 
   create() {
-    api
-      .getScore()
-      .then((data) => {
-        this.add
-          .bitmapText(100, 10, 'arcade', 'RANK  NAME   SCORE')
-          .setTint(0xffffff);
-        data.result.sort((a, b) => {
-          if (a.score > b.score) {
-            return -1;
-          } if (b.score > a.score) {
-            return 1;
-          }
-          return 0;
-        });
-        for (let i = 1; i < 6; i += 1) {
+    document.getElementById('spinner').classList.remove('hidden');
+
+    setTimeout(() => {
+      api
+        .getScore()
+        .then((data) => {
           this.add
-            .bitmapText(
-              100,
-              60 + 50 * i,
-              'arcade',
-              ` ${i}    ${data.result[i].user}     ${data.result[i].score}`,
-            )
+            .bitmapText(100, 10, 'arcade', 'RANK  NAME   SCORE')
             .setTint(0xffffff);
-        }
-      })
-      .catch((e) => {
-        const input = document.getElementById('space-shooter-game');
-        input.innerText = e;
-      });
+          data.result.sort((a, b) => {
+            if (a.score > b.score) {
+              return -1;
+            }
+            if (b.score > a.score) {
+              return 1;
+            }
+            return 0;
+          });
+          for (let i = 1; i < 6; i += 1) {
+            this.add
+              .bitmapText(
+                100,
+                60 + 50 * i,
+                'arcade',
+                ` ${i}    ${data.result[i].user}     ${data.result[i].score}`
+              )
+              .setTint(0xffffff);
+          }
+          document.getElementById('spinner').classList.add('hidden');
+        })
+        .catch((e) => {
+          const input = document.getElementById('space-shooter-game');
+          input.innerText = e;
+        });
+    }, 3000);
 
     this.add
       .bitmapText(this.game.config.width * 0.5, 500, 'arcade', 'PRESS ENTER!')
@@ -44,7 +50,7 @@ export default class GameOverScene extends Phaser.Scene {
 
     // Key Bindings
     this.keyENTER = this.input.keyboard.addKey(
-      Phaser.Input.Keyboard.KeyCodes.ENTER,
+      Phaser.Input.Keyboard.KeyCodes.ENTER
     );
   }
 
